6c92e7ae3cdcc605d8c8870cbd6a384c742663c6
foziaShareen <foziashareen1@gmail.com>
2023-01-12 10:57:02 +0500

    updtade blog

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import BlogListView,BlogDetailView,BlogCreateView
+from .views import BlogListView,BlogDetailView,BlogCreateView,BlogUpdateView
 urlpatterns = [
+    path('post/<int:pk>/edit/',BlogUpdateView.as_view(),name='post_edit'),
     path('post/new/', BlogCreateView.as_view(), name='post_new'),
     path('post/<int:pk>/', BlogDetailView.as_view(),
         name='post_detail'),
--- blog/views.py
+++ blog/views.py
@@ -1,17 +1,26 @@
-from django.views.generic import ListView,DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView,UpdateView 
+
+
 from .models import Post
+
+
 class BlogListView(ListView):
     model = Post
     template_name = 'home.html'
 
 
-class BlogDetailView(DetailView):  
+class BlogDetailView(DetailView):
     model = Post
     template_name = 'post_detail.html'
+class BlogCreateView(CreateView):
+    model=Post
+    template_name='post_new.html'
+    fields=['title','author','body']
+class BlogUpdateView(UpdateView):
+    model=Post
+    template_name='post_edit.html'
+    fields=['title','body']   
 
 
-class BlogCreateView(CreateView):  # new
-    model = Post
-    template_name = 'post_new.html'
-    fields = ['title', 'author', 'body']
+  
